1f56032e2b3d7a751b86c5ded1eb524f4bc364f9
wangwq8 <wangwq8@midea.com>
2020-03-23 15:56:39 +0800

    leetcode 876. 链表的中间结点

---
+++ example/src/com/xiaoqiang/leetcode/LeetCode876.java
@@ -0,0 +1,100 @@
+package com.xiaoqiang.leetcode;
+
+import jdk.nashorn.internal.objects.annotations.Where;
+
+import java.util.List;
+
+/**
+ * 876. 链表的中间结点
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ * 示例 2：
+ *
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ *  
+ *
+ * 提示：
+ *
+ * 给定链表的结点数介于 1 和 100 之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 解题思路：
+ * 定义快慢两个指针，从头结点开始，慢指针每次向后移动一个结点，快指针每次向后移动两个结点。
+ * 当链表奇数个节点，快指针刚好在最后一个节点， 慢指针就是中间节点
+ *当链表偶数个节点，快指针刚好在最后一个节点的next==null, 慢指针刚刚在中间节点
+ *
+ */
+public class LeetCode876 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode head2 = new ListNode(2);
+        ListNode head3 = new ListNode(3);
+        ListNode head4 = new ListNode(4);
+        head.setNext(head2);
+        head2.setNext(head3);
+        head3.setNext(head4);
+        ListNode middleNode = middleNode(head);
+        System.out.println(middleNode.val);
+    }
+    public static ListNode middleNode(ListNode head) {
+        if(head == null && head.next == null){
+            return head;
+        }
+        ListNode fastNode = head;
+        ListNode slowNode = head;
+        while (fastNode != null && fastNode.next != null){
+            fastNode = fastNode.next.next;
+            slowNode = slowNode.next;
+        }
+        return slowNode;
+    }
+}
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class  ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
