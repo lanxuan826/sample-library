cae3942101b3f5ce41cbf399f05427a944f6baf8
lanxuan826 <1011073335@qq.com>
2020-03-30 15:21:23 +0800

    LeetCode 面试题62. 圆圈中最后剩下的数字

---
+++ example/src/com/xiaoqiang/leetcode/LeetCodeInterview62.java
@@ -0,0 +1,60 @@
+package com.xiaoqiang.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 面试题62. 圆圈中最后剩下的数字
+ * 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+ *
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入: n = 5, m = 3
+ * 输出: 3
+ * 示例 2：
+ *
+ * 输入: n = 10, m = 17
+ * 输出: 2
+ *
+ *
+ * 限制：
+ *
+ * 1 <= n <= 10^5
+ * 1 <= m <= 10^6
+ *
+ *
+ */
+public class LeetCodeInterview62 {
+
+    public static void main(String[] args) {
+        int n = 5, m=3;
+        System.out.println(lastRemaining(n,3));
+    }
+
+    public static int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i< n; i++){
+            list.add(i);
+        }
+        int index =0;
+        while (n > 1){
+            index = (index+m-1) % n; //循环删除
+            System.out.println("删除的下标："+index+",值："+list.get(index));
+            list.remove(index);
+            n--;
+        }
+        return list.get(0);
+    }
+
+    public static int lastRemaining2(int n, int m){
+        int result = 0;
+        for (int i=2; i<=n; i++){
+            result = (result+m)% i;
+        }
+        return result;
+    }
+}
